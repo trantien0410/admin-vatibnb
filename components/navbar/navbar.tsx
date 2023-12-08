4623afb7c0ff7617618e6bf1198cf85e44c24ac0
Tien <tien04100@gmail.com>
2023-12-08 21:53:10 +0700

    add store switcher

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -5,6 +5,7 @@ import getCurrentUser from "@/actions/get-current-user";
 import { redirect } from "next/navigation";
 import { MainNav } from "@/components/main-nav";
 import { ThemeToggle } from "@/components/theme-toggle";
+import StoreSwitcher from "../store-switcher";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -20,6 +21,7 @@ const Navbar: React.FC<NavbarProps> = async ({ currentUser }) => {
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <Logo />
+        <StoreSwitcher />
         <MainNav className="mx-10" />
         <div className="ml-auto flex items-center space-x-4">
           <ThemeToggle />
---
+++ components/store-switcher.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useParams, usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Check, ChevronsUpDown, Store as StoreIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+  typeof PopoverTrigger
+>;
+
+interface StoreSwitcherProps extends PopoverTriggerProps {}
+
+const stores = [
+  {
+    value: "hotels",
+    label: "Hotels",
+  },
+  {
+    value: "vehicles",
+    label: "Vehicles",
+  },
+];
+
+export default function StoreSwitcher({ className }: StoreSwitcherProps) {
+  const router = useRouter();
+  const params = useParams();
+  const pathname = usePathname();
+  const isCarPage = pathname?.startsWith(`/${params?.adminId}/vehicles`);
+  const [open, setOpen] = useState(false);
+
+  const currentStore = stores.find(
+    (item) => item.value === (isCarPage ? "vehicles" : "hotels")
+  );
+
+  const onStoreSelect = (store: { value: string; label: string }) => {
+    setOpen(false);
+    router.push(`/${params?.adminId}/${store.value}`);
+  };
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          role="combobox"
+          aria-expanded={open}
+          aria-label="Select a store"
+          className={cn("w-[200px] justify-between", className)}
+        >
+          <StoreIcon className="mr-2 h-4 w-4" />
+          {currentStore?.label}
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandInput placeholder="Search Store..." />
+            <CommandEmpty>No Store Found</CommandEmpty>
+            <CommandGroup heading="Store">
+              {stores.map((store) => (
+                <CommandItem
+                  key={store.value}
+                  onSelect={() => onStoreSelect(store)}
+                  className="text-sm"
+                >
+                  <StoreIcon className="mr-2 h-4 w-4" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      currentStore?.value === store.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
